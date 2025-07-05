91ee0c212dc57040b3f4e53b8e464e8edb4eb3a9
kentheboy <foryoukentheboy1020@gmail.com>
2025-07-05 22:17:38 +0800

    feat: enhance Slack message handling with detailed status updates and new message templates

--- index.js
+++ index.js
@@ -9,35 +9,90 @@ function postRequest(data) {
     const options = {
       host: url.hostname,
       path: url.pathname,
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
-    
+
     // Log the data being sent to Slack
     const req = https.request(options, (res) => {
-      let responseBody = '';
-      res.on('data', (chunk) => {
+      let responseBody = "";
+      res.on("data", (chunk) => {
         responseBody += chunk;
       });
-      res.on('end', () => {
-        console.log('Slack response body:', responseBody);
+      res.on("end", () => {
+        console.log("Slack response body:", responseBody);
         if (res.statusCode < 200 || res.statusCode >= 300) {
-          console.error(`Slack request error log: ${JSON.stringify(responseBody)}`);
-          return reject(`Slack request failed with status code: ${res.statusCode}`);
+          console.error(
+            `Slack request error log: \n${JSON.stringify(responseBody)}`
+          );
+          return reject(
+            `Slack request failed with status code: ${res.statusCode}`
+          );
         }
         resolve(`Slack status: ${res.statusCode}`);
       });
     });
 
-    req.on('error', (e) => {
+    req.on("error", (e) => {
       reject(`Request failed: ${e.message}`);
     });
 
     req.write(JSON.stringify(data));
     req.end();
-  })
+  });
+}
+
+function messageBuilder(event) {
+  const snsMessageRaw = event.Records[0].Sns.Message;
+  const timestamp = new Date(event.Records[0].Sns.Timestamp).getTime() / 1000;
+
+  console.log("Row message:", JSON.stringify(snsMessageRaw));
+
+  const branch = snsMessageRaw.detail.branchName;
+  const status = snsMessageRaw.detail.jobStatus;
+  // environment link
+  if (!branch) {
+    const link = `<https://${process.env.SLACK_WEBHOOK_URL}|${process.env.SLACK_WEBHOOK_URL}>`;
+  } else {
+    const link = `<https://${branch}.${process.env.SLACK_WEBHOOK_URL}|${branch}.${process.env.SLACK_WEBHOOK_URL}>`;
+  }
+
+  let message = null;
+  let appendText = null;
+  switch (status) {
+    case "STARTED":
+      message = require("./started_message.json");
+      message.attachments[0].ts = timestamp;
+      return message;
+    case "FAILED":
+      message = require("./faild_message.json");
+      appendText = {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `\`\`\`${JSON.stringify(snsMessageRaw)}\`\`\``,
+        },
+      };
+      message.attachments[0].blocks.push(appendText);
+      message.attachments[0].ts = timestamp;
+      return message;
+    case "SUCCEED":
+      message = require("./success_message.json");
+      appendText = {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: link,
+        },
+      };
+      message.attachments[0].blocks.push(appendText);
+      message.attachments[0].ts = timestamp;
+      return message;
+    default:
+      return { color: "info", text: "Build status unknown" };
+  }
 }
 
 exports.handler = async (event) => {
@@ -48,34 +103,11 @@ exports.handler = async (event) => {
       return;
     }
 
-    const snsMessageRaw = event.Records[0].Sns.Message;
-    const timestamp = new Date(event.Records[0].Sns.Timestamp).getTime() / 1000;
-
-    const message = typeof snsMessageRaw === "string"
-      ? snsMessageRaw.replace(/^"|"$/g, "") // remove leading/trailing quotes if needed
-      : JSON.stringify(snsMessageRaw);
-
-    let color = "info";
-    if (message.includes("FAILED")) {
-      color = "danger";
-    } else if (message.includes("SUCCEED")) {
-      color = "good";
-    }
-
-    const slackMessage = {
-      attachments: [
-        {
-          color,
-          text: message,
-          ts: timestamp,
-        },
-      ],
-    };
+    const slackMessage = await messageBuilder(event);
 
     const result = await postRequest(slackMessage);
     console.log("Slack notification sent:", result);
-
   } catch (err) {
     console.error("Lambda Error:", err);
   }
-}
+};
